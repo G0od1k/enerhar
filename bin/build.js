b7d8a7637834d417c1cc1adc3d4a9d510e9b1ccb
G0od1k <g0cd1k@gmail.com>
2022-11-25 19:32:31 +0200

    Builder for multiple lang

--- bin/build.js
+++ bin/build.js
@@ -6,40 +6,54 @@ const webpack = require(`webpack`)
 
 const pkg = require(`../package.json`)
 
-webpack(
-    {
-        mode: `production`,
-        entry: `./src/index.js`,
-        output: {
-            path: path.resolve(__dirname, `../build`),
-            filename: `${pkg.name}@${pkg.version}.js`,
-        },
-        target: `node`,
-        externalsPresets: {
-            node: true,
-        },
-        resolve: {
-            extensions: [".json", ".js"],
-        },
-        optimization: {
-            minimizer: [
-                new TerserPlugin({
-                    extractComments: false,
+fs.rmSync(path.resolve(__dirname, `../build`), { recursive: true, force: true })
+
+const langs = fs.readdirSync(`./src/lang`).map((x) => x.slice(0, -5))
+
+console.log(
+    `Languages: ${langs.map((x) => `\x1b[93m${x}\x1b[0m`).join(`, `)}\n`
+)
+
+langs.forEach((lang) => {
+    webpack(
+        {
+            mode: `production`,
+            entry: `./src/index.js`,
+            output: {
+                path: path.resolve(__dirname, `../build`),
+                filename: `${pkg.name}@${pkg.version}[${lang}].js`,
+            },
+            target: `node`,
+            externalsPresets: {
+                node: true,
+            },
+            resolve: {
+                extensions: [".json", ".js"],
+            },
+            optimization: {
+                minimizer: [
+                    new TerserPlugin({
+                        extractComments: false,
+                    }),
+                ],
+            },
+            plugins: [
+                new webpack.NormalModuleReplacementPlugin(
+                    /lang\/\w+/,
+                    `./lang/${lang}.json`
+                ),
+                new webpack.BannerPlugin({
+                    banner: fs.readFileSync(`LICENSE`, `utf-8`),
                 }),
             ],
         },
-        plugins: [
-            new webpack.BannerPlugin({
-                banner: fs.readFileSync(`LICENSE`, `utf-8`),
-            }),
-        ],
-    },
-    (err, stats) => {
-        if (err) {
-            console.log(err)
-            return
-        }
+        (err, stats) => {
+            if (err) {
+                console.log(err)
+                return
+            }
 
-        console.log(stats.toString({ chunks: false, colors: true }))
-    }
-)
+            console.log(stats.toString({ chunks: false, colors: true }) + `\n`)
+        }
+    )
+})
